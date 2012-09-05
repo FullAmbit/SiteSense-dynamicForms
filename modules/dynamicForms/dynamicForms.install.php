011e07eff05745d530854ef22f60b6e1d766c5fb
Zach Bloomquist <zbloomquist@fullambit.net>
2012-09-05 18:17:18 -0400

    updated vnumber

--- modules/dynamicForms/dynamicForms.install.php
+++ modules/dynamicForms/dynamicForms.install.php
@@ -26,7 +26,7 @@ function dynamicForms_settings() {
 	return array(
 		'name'      => 'dynamicForms',
 		'shortName' => 'dynamic-forms',
-		'version'   => '1.0',
+		'version'   => '1.0.1',
 	);
 }
 function dynamicForms_install($db, $drop=false, $firstInstall=false, $lang='en_us') {
