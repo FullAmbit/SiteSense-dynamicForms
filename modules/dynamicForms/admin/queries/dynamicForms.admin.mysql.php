7c15fbf74cf9b59c6a52a3a1c4bf8bc7c52f6421
Zach Bloomquist <zbloomquist@fullambit.net>
2012-11-17 10:57:20 -0500

    Removing unnecessary queries

--- modules/dynamicForms/admin/queries/dynamicForms.admin.mysql.php
+++ modules/dynamicForms/admin/queries/dynamicForms.admin.mysql.php
@@ -22,15 +22,8 @@
 * @copyright  Copyright (c) 2011 Full Ambit Media, LLC (http://www.fullambit.com)
 * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 */
-/*
-	!table! = $tableName
-	!prefix! = dynamicPDO::tablePrefix
-*/
 function admin_dynamicForms_addQueries() {
 	return array(
-		'getAllFormIds' => '
-			SELECT id FROM !prefix!forms!lang!
-		',
 		'getAllForms' => '
 			SELECT * FROM !prefix!forms!lang! ORDER BY id DESC
 		',
